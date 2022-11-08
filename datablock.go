8e2ec18635cfaf2527fd1fefacfe87df65eed941
jurgen.kluft <jurgen.kluft@gmail.com>
2022-11-08 20:20:57 +0800

    * Block; write functions

--- datablock.go
+++ datablock.go
@@ -3,12 +3,17 @@ package datastream
 import (
 	"bytes"
 	"encoding/binary"
+	"math"
 )
+
+// TODO going through bytes.Buffer and binary.Write is not the most efficient way to do this.
 
 type Block struct {
 	This      Pointer
+	Size      int64
 	Endian    binary.ByteOrder
 	Alignment int
+	PutBuffer []byte
 	Writer    *bytes.Buffer
 	Pointers  []Pointer
 }
@@ -16,11 +21,33 @@ type Block struct {
 func NewBlock(ptr Pointer, endian binary.ByteOrder, alignment int) *Block {
 	return &Block{
 		This:      ptr,
+		Size:      0,
 		Endian:    endian,
 		Alignment: alignment,
+		PutBuffer: make([]byte, 16),
 		Writer:    new(bytes.Buffer),
 		Pointers:  make([]Pointer, 0),
 	}
+}
+
+func Align(value int64, alignment int) int64 {
+	return (value + int64(alignment-1)) &^ int64(alignment-1)
+}
+
+func (d *Block) Close() {
+	d.Size = int64(d.Writer.Len())
+	d.Size = Align(d.Size, d.Alignment)
+}
+
+// Align a block
+func (d *Block) Align(alignment int) {
+	// Write out '0' bytes to align the block
+	pos := d.Writer.Len()
+	gap := Align(int64(pos), alignment) - int64(pos)
+	for i := int64(0); i < gap; i++ {
+		d.PutBuffer[i] = 0
+	}
+	d.WriteBytes(d.PutBuffer[:gap])
 }
 
 func (d *Block) Write(p []byte) {
@@ -30,7 +57,9 @@ func (d *Block) Write(p []byte) {
 func (d *Block) WriteString(s string) {
 	strbytes := []byte(s)
 	strlen := len(strbytes)
-	d.WriteInt32(int32(strlen))
+	d.Align(4)
+	d.Endian.PutUint32(d.PutBuffer, uint32(strlen))
+	d.WriteBytes(d.PutBuffer[:4])
 	d.WriteBytes(strbytes)
 }
 
@@ -39,56 +68,61 @@ func (d *Block) WriteBytes(b []byte) {
 }
 
 func (d *Block) WriteInt8(i int8) {
-	binary.Write(d.Writer, d.Endian, i)
+	d.Writer.WriteByte(byte(i))
 }
 
 func (d *Block) WriteInt16(i int16) {
-	// TODO make sure to align
-	binary.Write(d.Writer, d.Endian, i)
+	d.Align(2)
+	d.Endian.PutUint16(d.PutBuffer, uint16(i))
+	d.WriteBytes(d.PutBuffer[:2])
 }
 
 func (d *Block) WriteInt32(i int32) {
-	// TODO make sure to align
-	binary.Write(d.Writer, d.Endian, i)
+	d.Align(4)
+	d.Endian.PutUint32(d.PutBuffer, uint32(i))
+	d.WriteBytes(d.PutBuffer[:4])
 }
 
 func (d *Block) WriteInt64(i int64) {
-	// TODO make sure to align
-	binary.Write(d.Writer, d.Endian, i)
+	d.Align(8)
+	d.Endian.PutUint64(d.PutBuffer, uint64(i))
+	d.WriteBytes(d.PutBuffer[:8])
 }
 
 func (d *Block) WriteUInt8(i uint8) {
-	binary.Write(d.Writer, d.Endian, i)
+	d.Writer.WriteByte(byte(i))
 }
 
 func (d *Block) WriteUInt16(i uint16) {
-	// TODO make sure to align
-	binary.Write(d.Writer, d.Endian, i)
+	d.Align(2)
+	d.Endian.PutUint16(d.PutBuffer, i)
+	d.WriteBytes(d.PutBuffer[:2])
 }
 
 func (d *Block) WriteUInt32(i uint32) {
-	// TODO make sure to align
-	binary.Write(d.Writer, d.Endian, i)
+	d.Align(4)
+	d.Endian.PutUint32(d.PutBuffer, i)
+	d.WriteBytes(d.PutBuffer[:4])
 }
 
 func (d *Block) WriteUInt64(i uint64) {
-	// TODO make sure to align
-	binary.Write(d.Writer, d.Endian, i)
+	d.Align(8)
+	d.Endian.PutUint64(d.PutBuffer, i)
+	d.WriteBytes(d.PutBuffer[:8])
 }
 
 func (d *Block) WriteFloat(f float32) {
-	// TODO make sure to align
-	binary.Write(d.Writer, d.Endian, f)
+	d.WriteUInt32(math.Float32bits(f))
 }
 
 func (d *Block) WriteDouble(f float64) {
-	// TODO make sure to align
-	binary.Write(d.Writer, d.Endian, f)
+	d.WriteUInt64(math.Float64bits(f))
 }
 
 func (d *Block) WritePtr(ptr Pointer) {
-	// TODO make sure to align
+	d.Align(8)
 	d.Pointers = append(d.Pointers, ptr)
 	ptr.Offset = int64(d.Writer.Len())
-	binary.Write(d.Writer, d.Endian, ptr.Offset)
+	d.Endian.PutUint64(d.PutBuffer, uint64(ptr.Offset))
+	d.WriteBytes(d.PutBuffer[:8])
 }
